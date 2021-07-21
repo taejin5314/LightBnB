eeedf722e0e8fdb6c15890f147c2898ea4f1db35
Taejin Jung <taejin5314@gmail.com>
2021-07-21 17:39:30 +0000

    create new tables(commented)

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -46,3 +46,19 @@ CREATE TABLE property_reviews (
   message TEXT
 );
 
+-- CREATE TABLE guest_reviews (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   reservation_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+--   rating SMALLINT NOT NULL DEFAULT 0,
+--   message TEXT
+-- );
+ 
+-- CREATE TABLE rates (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   start_date DATE NOT NULL,
+--   end_date DATE NOT NULL,
+--   cost_per_night INTEGER NOT NULL DEFAULT 0,
+--   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+-- ); 
